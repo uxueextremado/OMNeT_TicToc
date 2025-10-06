72965db61b42a683e072d559adcfcfc8102c705c
Uxue Extremado <uxueex@gmail.com>
2025-10-06 22:37:11 +0200

    Create txc2.cc

---
+++ txc2.cc
@@ -0,0 +1,38 @@
+#include <string.h>
+#include <omnetpp.h>
+
+using namespace omnetpp;
+
+/**
+ * In this step we add some debug messages to Txc1. When you run the
+ * simulation in the OMNeT++ Qtenv GUI, the log will appear in the bottom
+ * panel of the Qtenv window. To see only the log from `tic' or `toc' alone,
+ * go into them by double-clicking their icons, and the bottom panel will
+ * be filtered accordingly. (You can go back with the up arrow button on
+ * the toolbar.)
+ */
+class Txc2 : public cSimpleModule
+{
+  protected:
+    virtual void initialize() override;
+    virtual void handleMessage(cMessage *msg) override;
+};
+
+Define_Module(Txc2);
+
+void Txc2::initialize()
+{
+    if (strcmp("tic", getName()) == 0) {
+        // The `ev' object works like `cout' in C++.
+        EV << "Sending initial message\n";
+        cMessage *msg = new cMessage("tictocMsg");
+        send(msg, "out");
+    }
+}
+
+void Txc2::handleMessage(cMessage *msg)
+{
+    // msg->getName() is name of the msg object, here it will be "tictocMsg".
+    EV << "Received message `" << msg->getName() << "', sending it out again\n";
+    send(msg, "out");
+}
